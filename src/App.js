0832e5b93a14bfcc098d25dcf00ff04e06c09645
asifrahaman13 <asifrahaman162@gmail.com>
2022-12-14 11:21:55 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
               <Route path="/asifrahamanportfolio" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Home/>} />
             </Routes>
             <Footer />
           </BrowserRouter>
